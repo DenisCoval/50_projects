2b87fd5a987f43975f41e8fa5f6afc4cb57a83cf
Denis Coval <denispinheiro08@gmail.com>
2021-08-02 12:14:45 +0100

    first commit

---
+++ progress_steps/script.js
@@ -0,0 +1,58 @@
+const progress = document.getElementById('progress')
+const prev = document.getElementById('prev')
+const next = document.getElementById('next')
+
+// const prev = document.querySelector('#prev')
+// const next = document.querySelector('#next')
+const circles = document.querySelectorAll('.circle')
+// const circles = document.getElementsByClassName('circle')
+
+let currentActive = 1
+
+next.addEventListener('click', () => {
+    currentActive++
+
+    if (currentActive > circles.length) {
+        currentActive = circles.length
+    }
+    update()
+})
+
+prev.addEventListener('click', () => {
+    currentActive--
+
+    if (currentActive < 1) {
+        currentActive = 1
+    }
+    update()
+})
+function update() {
+    //     circles.forEach((circle, index) => {
+    //         if (index < currentActive) {
+    //             circle.classList.add('active')
+    //         } else {
+    //             circle.classList.remove('active')
+    //         }
+    //     })
+
+    // #### Opção 2 ####
+    for (const [index, circle] of circles.entries())
+        if (index < currentActive) {
+            circle.classList.add('active')
+        } else {
+            circle.classList.remove('active')
+        }
+
+    const actives = document.querySelectorAll('.active')
+
+    progress.style.width =
+        ((actives.length - 1) / (circles.length - 1)) * 100 + '%'
+    if (currentActive == 1) {
+        prev.disabled = true
+    } else if (currentActive == circles.length) {
+        next.disabled = true
+    } else {
+        prev.disabled = false
+        next.disabled = false
+    }
+}
