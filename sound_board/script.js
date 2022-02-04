62925aca59e361417ae13780b43f3554d0d01bd9
Denis Coval <denispinheiro08@gmail.com>
2022-02-04 15:42:42 +0000

    Complete

---
+++ sound_board/script.js
@@ -0,0 +1,25 @@
+const sounds = ['um', 'dois', 'tres', 'quatro', 'cinco', 'seis']
+const teste = document.createElement('teste')
+
+sounds.forEach(sound => {
+    const btn = document.createElement('button')
+    btn.classList.add('btn')
+
+    btn.innerText = sound
+
+    btn.addEventListener('click', () => {
+        stopSongs()
+
+        document.getElementById(sound).play()
+    })
+
+    const buttons = document.getElementById('buttons')
+    buttons.appendChild(btn)
+})
+function stopSongs() {
+    sounds.forEach(sound => {
+        const song = document.getElementById(sound)
+        song.pause()
+        song.currentTime = 0
+    })
+}
